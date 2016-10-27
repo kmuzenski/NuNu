33a7caed12f3012037208c691911fb6e3d625a08
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2016-10-27 15:27:43 -0500

    bootstrap

--- index.php
+++ index.php
@@ -11,7 +11,15 @@
 <br><br><br><br><br><br>
 <center>
 <h1>NUNU's TOWING AND SALVAGE</h1>
-<img src="assets/img/NNT.jpg">
+<div class="container">
+<div class="row">
+
+<div class="col-xs-12">
+	<img src="assets/img/NNT.jpg">
+</div>
+</div>
+</div>
+
 </center>
 
 
