4fa9977974aecaed679707045216624c42bc4cc9
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-14 23:03:22 -0500

    + map

--- contact.php
+++ contact.php
@@ -26,6 +26,14 @@
 			</center>
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<br><center>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d186573.42106320683!2d-88.10751520032713!3d43.058056891445986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1508040073984" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</center>
+		</div>
+	</div>
+
 </div> <!--container -->
 
 <br><br><br>
