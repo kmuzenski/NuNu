8225eb02f61bda72e751d4efc1a1bd7dc4dbd17a
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-14 22:22:21 -0500

    +services page

--- index.php
+++ index.php
@@ -12,21 +12,23 @@
 <center>
 <h1>NUNU's TOWING AND SALVAGE</h1>
 <p>Servicing the greater Milwaukee area since 2015. 24 hour low cost towing. We pay top dollar for junk/salvage cars.See our <a href="services.php" title="services" alt="services"> Services </a> for more information. Call us today! 414-777-7777<p>
-
+</center>
 <div class="container">
 <div class="row">
 <div class="col-xs-12">
+<center>
 <img src="assets/img/NNT.jpg" width="75%">
-</div>
-
-</div>
-
-</div>
-
 </center>
+</div>
+
+</div>
+
+</div>
 
 
-<br><br><br><br><br>
+
+
+<br><br><br>
 <?php require_once('footer.php'); ?>
 
 
--- services.php
+++ services.php
@@ -8,11 +8,33 @@
 
 
 <?php require_once('nav.php'); ?>
-<br><br><br><br><br><br>
+<br><br><br>
 
-<h1> ABOUT OUR COMPANY </h1>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<h1> NuNu's Services </h1>
+			<br>
+		</div>
+	</div>
 
-<br><br><br><br><br>
+	<div class="row">
+		<div class="col-xs-push-6 col-md-6 col-lg-6">
+			<img src="assets/img/tow.jpg" width="75%">
+		</div>
+		<div class="col-xs-pull-6 col-md-6 col-lg-6">
+			<p>-24 Hour Towing Services<br>
+			   -We buy JUNK cars for CASH<br>
+			   -MUST have clean title on hand<br>
+			   -No current Liens within 10 years<br>
+			</p>
+		</div>
+	</div>
+
+</div> <!-- container --> 
+
+
+<br><br><br>
 <?php require_once('footer.php'); ?>
 
 
