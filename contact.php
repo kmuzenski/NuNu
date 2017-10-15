43ea3a814668cb3490b0450f7da7a4c5965c7f0e
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-14 22:57:25 -0500

    + contact page

--- contact.php
+++ contact.php
@@ -8,11 +8,23 @@
 
 
 <?php require_once('nav.php'); ?>
-<br><br><br><br><br><br>
-<h1> CONTACT US </h1> 
+<br><br><br>
+<center><h1> Contact NuNu's Towing and Salvage LLC. </h1></center>
 
+<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<h2 alt="phone number" title="phone number">Phone: 414-777-7777</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12 col-md-12 col-lg-12">
+			<a href="https://www.facebook.com/Nunus-towing-salvage-llc-1045056545518539/" alt="facebook" title="facebook">Like &amp; Follow our Page on Facebook</a>
+		</div>
+	</div>
+</div> <!--container -->
 
-<br><br><br><br><br>
+<br><br><br>
 <?php require_once('footer.php'); ?>
 
 
