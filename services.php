eb5ff7756bd6661dd8b507cb409487c4bdec7914
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-14 22:26:35 -0500

    styles fixes

--- services.php
+++ services.php
@@ -10,23 +10,24 @@
 <?php require_once('nav.php'); ?>
 <br><br><br>
 
+<center>
+	<h1>Services</h1>
+	<br>
+</center>
+
 <div class="container">
-	<div class="row">
-		<div class="col-xs-12 col-md-12 col-lg-12">
-			<h1> NuNu's Services </h1>
-			<br>
-		</div>
-	</div>
 
 	<div class="row">
 		<div class="col-xs-push-6 col-md-6 col-lg-6">
-			<img src="assets/img/tow.jpg" width="75%">
+			<img src="assets/img/tow.jpg" width="50%">
 		</div>
 		<div class="col-xs-pull-6 col-md-6 col-lg-6">
+			<center>
 			<p>-24 Hour Towing Services<br>
 			   -We buy JUNK cars for CASH<br>
 			   -MUST have clean title on hand<br>
 			   -No current Liens within 10 years<br>
+			</center>
 			</p>
 		</div>
 	</div>
