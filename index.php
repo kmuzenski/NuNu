1f6ea236260c76deb850fce2b0d8fbda27518312
Kathryn Muzenski <kathrynmuzenski@yahoo.com>
2017-10-14 21:58:34 -0500

    updating pages

--- index.php
+++ index.php
@@ -8,17 +8,15 @@
 
 
 <?php require_once('nav.php'); ?>
-<br><br><br><br><br><br>
+<br><br><br>
 <center>
 <h1>NUNU's TOWING AND SALVAGE</h1>
-<br>
-<p>Call us today! 414-777-7777<p>
+<p>Servicing the greater Milwaukee area since 2015. 24 hour low cost towing. We pay top dollar for junk/salvage cars.See our <a href="services.php" title="services" alt="services"> Services </a> for more information. Call us today! 414-777-7777<p>
+
 <div class="container">
-
 <div class="row">
-
 <div class="col-xs-12">
-<img src="assets/img/NNT.jpg">
+<img src="assets/img/NNT.jpg" width="75%">
 </div>
 
 </div>
